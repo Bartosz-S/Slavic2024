934d15f03baa695dabd9f02fb7f89deeba1bc670
Dawid Kostrzewa <dawid.k2602@gmail.com>
2024-08-31 17:08:18 +0200

    Delete usless comment

--- Assets/Scripts/GameSystem/GameController.cs
+++ Assets/Scripts/GameSystem/GameController.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         foreach(EnemyVision enemyVision in FindObjectsByType<EnemyVision>(FindObjectsSortMode.None))
